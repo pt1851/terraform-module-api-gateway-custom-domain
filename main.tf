7150c1015c08acc9960f789df3b376fb9e4a9be0
AkashSivakumar-Dev <82136973+AkashSivakumar-Dev@users.noreply.github.com>
2022-07-09 16:26:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # The domain name to use with api-gateway
 
 
-resource "time_sleep" "wait_15_seconds" {
-  create_duration = "15s"
+resource "time_sleep" "wait_60_seconds" {
+  create_duration = "60s"
 }
 
 
