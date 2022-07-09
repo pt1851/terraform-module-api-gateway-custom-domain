e45c60a666450e74bd946ce25f84e34285add8d5
AkashSivakumar <akash@pearlthoughts>
2022-07-09 17:29:11 +0530

    Changed custom domain type from edge to regional

--- main.tf
+++ main.tf
@@ -22,14 +22,9 @@ resource "aws_api_gateway_domain_name" "domain_name" {
 
 resource "aws_route53_record" "sub_domain" {
   name    = var.sub_domain
-  type    = "A"
+  type    = "CNAME"
+  value   = aws_api_gateway_domain_name.domain_name.regional_domain_name 
   zone_id = var.zone_id
-
-  alias {
-    name                   = aws_api_gateway_domain_name.domain_name.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.domain_name.cloudfront_zone_id
-    evaluate_target_health = false
-  }
 }
 
 resource "aws_acm_certificate" "cert" {
